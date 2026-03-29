cde2fe4d37acd07051a8d748e254058320e93630
Zt <lyoneidastu@hotmail.com>
2026-03-29 20:43:14 +0800

    feat: add example momentum strategy

--- tyche-client/tyche_client/__init__.py
+++ tyche-client/tyche_client/__init__.py
@@ -4,8 +4,10 @@ __version__ = "1.0.0"
 
 from .types import Tick, Quote, Trade, Bar, Order, OrderEvent, Ack, Position, Risk
 from .serialization import encode, decode
+from .module import Module
 
 __all__ = [
+    "Module",
     "Tick",
     "Quote",
     "Trade",
